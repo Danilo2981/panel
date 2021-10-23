505136386d76e7f29a473f38027e3a1532b5d7f9
Danilo <danilo.vega@hotmail.es>
2021-10-23 00:06:29 -0500

    users show

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,9 +1,49 @@
 @extends('admin.dashboard')
 
-@section('title', "Usuario {$id}")
+@section('title', "Usuario {$user->id}")
 
 @section('content')
-    <h1>Usuario #{{ $id }}</h1>
+    
+<!-- Profile Card -->
+<div>
+    <h2>Usuario #{{ $user->id }}</h2>
+    <div class="md:grid grid-cols-4 grid-rows-2  bg-white gap-2 p-4 rounded-xl">
+         <div class="md:col-span-1 h-48 shadow-xl ">
+                 <div class="flex w-full h-full relative">
+                     <img src={{ $user->image }} class="w-44 h-44 m-auto" alt=""> 
+                 </div>
+         </div>
+         <div class="md:col-span-3 h-48 shadow-xl p-4 space-y-2 p-3">
+                 <div class="flex ">
+                     <span
+                         class="text-sm border bg-blue-50 font-bold uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">Name:</span>
+                     <input 
+                         class="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
+                         type="text" value={{ $user->name }}  readonly/>
+                 </div>
+                 <div class="flex ">
+                     <span
+                         class="text-sm border bg-blue-50 font-bold uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">Email:</span>
+                     <input 
+                         class="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
+                         type="text" value={{ $user->email }}  readonly/>
+                 </div>
+                  <div class="flex ">
+                     <span
+                         class="text-sm border bg-blue-50 font-bold uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">Role:</span>
+                     <input 
+                         class="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
+                         type="text" value={{ $user->is_admin }}  readonly/>
+                 </div>
+         </div>
+         <div class="md:col-span-3 h-48 shadow-xl p-4 space-y-2 hidden md:block">
+             <h3 class="font-bold uppercase"> Profile Description</h3>
+             <p class=""> 
+                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget laoreet diam, id luctus lectus. Ut consectetur nisl ipsum, et faucibus sem finibus vitae. Maecenas aliquam dolor at dignissim commodo. Etiam a aliquam tellus, et suscipit dolor. Proin auctor nisi velit, quis aliquet sapien viverra a. 
+             </p>
+         </div>
+             
+     </div>
+ </div>
 
-    Mostrando detalle del usuario: {{ $id }}
 @endsection
--- tests/Feature/UsersModuleTest.php
+++ tests/Feature/UsersModuleTest.php
@@ -2,26 +2,43 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UsersModuleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
-    public function it_loads_the_users()
+    public function it_shows_the_users_lsit()
     {
+        User::factory()->create(
+            ['name' => 'Joel']
+        );
+
+        User::factory()->create(
+            ['name' => 'Ellie']
+        );
+
         $this->get('/users')
         ->assertStatus(200)
-        ->assertSee('Usuarios');
+        ->assertSee('Listado de usuarios')
+        ->assertSee('Joel')
+        ->assertSee('Ellie');
     }
 
     /** @test */
-    public function it_loads_the_users_details_page()
+    public function it_displays_the_users_details()
     {
-        $this->get('/users/5')
+        $user = User::factory()->create([
+            'name' => 'Danilo Vega'
+        ]);
+
+        $this->get('/users/'.$user->id)
         ->assertStatus(200)
-        ->assertSee('Mostrando detalle del usuario: 5');
+        ->assertSee('Danilo Vega');
     }
 
     /** @test */
